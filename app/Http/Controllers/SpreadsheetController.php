0024971d4a5f6466a1e6d9cb43dd248077367ebc
raheelqazi326 <raheelqazi326@gmail.com>
2021-01-21 12:36:17 +0500

    working on yajra datatable

--- app/Http/Controllers/SpreadsheetController.php
+++ app/Http/Controllers/SpreadsheetController.php
@@ -7,4 +7,12 @@ use Illuminate\Http\Request;
 class SpreadsheetController extends Controller
 {
     //
+    public function index(){
+
+    }
+
+    public function uploadSpreadsheet(Request $request){
+        return response()->json($request->all());
+    }
+
 }
