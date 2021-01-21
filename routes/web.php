eaaee78aa3c52694cd5d8c93eebb9b5dc9b1c847
MudassirRaza912 <razamudassir912@gmail.com>
2021-01-21 15:26:30 +0500

    pull

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', 'App\Http\Controllers\AuthController@index')->name('login');
+Route::post('/auth/login', 'App\Http\Controllers\AuthController@login')->name('auth.login');
 
+/////////////////////////////////////// SHEET ROUTE //////////////////////////////////////////////
 Route::get('/sheet/list', 'App\Http\Controllers\SpreadsheetController@index')->name('sheet.list');
+
+
+/////////////////////////////////////// USER ROUTE //////////////////////////////////////////////
+Route::post('/user/sotore', 'App\Http\Controllers\UserController@store')->name('user.store');
+Route::get('/user/list', 'App\Http\Controllers\UserController@index')->name('user.list');
+Route::get('/user/create', 'App\Http\Controllers\UserController@create')->name('user.create');
 
 
 Route::get('/', function () {
