c0108687206ff0792f212de1eb3bae500ddd75ea
raheelqazi326 <raheelqazi326@gmail.com>
2021-01-26 11:28:57 +0500

    working

--- app/Http/Controllers/SpreadsheetController.php
+++ app/Http/Controllers/SpreadsheetController.php
@@ -15,10 +15,7 @@ class SpreadsheetController extends Controller
 
     public function uploadSpreadsheet(Request $request){
         $rows = $request->rows;
-        
-        
         // return response()->json($rows, 500);
-            
         foreach($rows as $row){        
             $spreadsheet = Spreadsheet::where('request_id', $row['Request Id'])->first();
             if(!empty($spreadsheet)){
@@ -27,7 +24,6 @@ class SpreadsheetController extends Controller
                 }
                 $spreadsheet->delete();
             }
-            
             $spreadsheet = new Spreadsheet;
             $spreadsheet->request_id = $row['Request Id']??$row['Request+Id']??"";
             $spreadsheet->date = date('Y-m-d', strtotime($row['Date']));
@@ -36,21 +32,7 @@ class SpreadsheetController extends Controller
             $spreadsheet->ward = $row['Ward'];
             $spreadsheet->request_grade = $row['Request Grade']??$row['Request+Grade']??"";
             $spreadsheet->status_id = 1;
-            $spreadsheet->save();
-        
-        
-            // $spreadsheet = new Spreadsheet;
-            // $spreadsheet->request_id = $row[0];
-            // $spreadsheet->date = date('Y-m-d', strtotime($row[1]));
-            // $spreadsheet->start = $row[2];
-            // $spreadsheet->end = $row[3];
-            // $spreadsheet->ward = $row[4];
-            // $spreadsheet->request_grade = $row[5];
-            // $spreadsheet->status_id = 1;
-            // $spreadsheet->save();
-        
-        
-            
+            $spreadsheet->save();            
         }
         return response()->json(['status' => true, 'message' => 'successfully uploaded']);
     }
